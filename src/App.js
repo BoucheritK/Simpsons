de1fed334b3bf603d45f1bb05ea763a1f0a10c13
Kevin Boucherit <boucherit.kevin@gmail.com>
2019-04-15 14:33:44 +0200

    chirac

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-import Quote from './Quote'
-import Quotes from './Quotes'
+import Quote from "./Quote";
+import Newlogo from "./Working";
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      speed: 20
+    }
+  }
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Simpsons Quotes </h1>
+         
+          <Newlogo url="https://risibank.fr/cache/stickers/d520/52080-full.png" classCSS="RoundSawBladeFast" textImgMissing="Round saw blade" />
+          <h1 class="App-title">CHIRAC</h1>
+          <p>
+            Hello folks, welcome !!
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </header>
-        < Quote 
-          quote="I believe the children are the future... Unless we stop them now!"
-          character="Homer Simpson"
-          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
-        />
-        < Quote
-          quote="Me fail English? That's unpossible"
-          character="Ralph Wiggum"
-          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
-        />
-        < Quotes />
+
+        <Quote />
       </div>
     );
   }
---
+++ src/other_logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OtherLogo = (props) => {
+    return (
+        <div>
+            <img style={props.namestyle} src="https://risibank.fr/cache/stickers/d520/52080-full.png" />
+        </div>
+    )
+}
+
+export default OtherLogo;
